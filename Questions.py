be252cc7dadd2ac674c20065ecb532911f91ceb2
Theo-Maiorca <tmaiorca@calpoly.edu>
2025-02-28 17:55:47 -0800

    Update Questions.py

--- Questions.py
+++ Questions.py
@@ -8,11 +8,6 @@ follow up =
 "negative": ["I'm here for you. Want to talk about it?", "That sounds tough. What happened?"], 
 "neutral": ["Gotcha. Anything interesting going on?", "Fair enough. Anything on your mind?"]
 }
-def main():
-    for question in questions:
-        print(question)
-        user_input = input()
-        response = analyze_input(user_input)
-        print(response)
+
 
   
