fbe15b4989289cd877add1edc31fc9ea474da0db
Theo-Maiorca <tmaiorca@calpoly.edu>
2025-03-05 13:47:46 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,37 @@
+from Partner_Class import Partner
+from Pet_Class import Pet
+
+def get_user_input(questions): 
+    responses = []
+    for question in questions: 
+        response.append)input(question))
+    return responses
+
+
 def main():
-    for question in questions:
-        print(question)
-        user_input = input()
-        response = analyze_input(user_input)
-        print(response)
+    #collecting input for making partner
+    name = input("Enter your partner's name: ")
+    age = int(input("Enter your partner's age: "))
+    gender = input("Enter your partner's gender: ")
+    appearance = input("Describe your partner's appearance: ")
+    personality = input("Describe your partner's personality: ")
+
+    #creates the user's partner
+    partner = Partner(name, age, gender, appearance, personality)
+    print(f"Meet your new partner: {partner}")
+
+    #collecting input for making pet
+    pet_name = input("Enter your pet's name: ")
+    pet_type = input("Enter the type of pet: ")
+
+    #create user's pet
+    pet = Pet(pet_name, pet_type)
+    print(f"You have a new pet: {pet}")
+
+
+#make sure script runs
+if __name__ == "__main__":
+    main()
+
 
 
