ead215582812e8918143dfb5f5865e8060b6f8b0
Theo-Maiorca <tmaiorca@calpoly.edu>
2025-03-05 13:26:23 -0800

    Update Pet_Class.py

--- Pet_Class.py
+++ Pet_Class.py
@@ -1,1 +1,7 @@
+class Pet:
+    def __init__(self, name, pet_type):
+        self.name = name
+        self.pet_type = pet_type
 
+    def __repr__(self):
+        return f"Pet(name={self.name}, pet_type={self.pet_type})"
