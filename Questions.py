dc81480db1d2fb1fc6c6e13a9a7a8c9a188cc906
Theo-Maiorca <tmaiorca@calpoly.edu>
2025-03-05 13:51:20 -0800

    Update Questions.py

--- Questions.py
+++ Questions.py
@@ -1,12 +1,24 @@
 
+questions = [
+    "How are you doing?", 
+    "What is your name?", 
+    "Can you tell me more?", 
+    "Is anything worrying you?"
+]
 
-questions = ["How are you doing?", "What is your name?", "Can you tell me more?", "Is anything worrying you?"]
-
-follow up questions based on response
-follow up = 
-{"positive": ["That's awesome! What made it so great?", "I love to hear that! Want to share more?"]
-"negative": ["I'm here for you. Want to talk about it?", "That sounds tough. What happened?"], 
-"neutral": ["Gotcha. Anything interesting going on?", "Fair enough. Anything on your mind?"]
+follow_up = {
+    "positive": [
+        "That's awesome! What made it so great?", 
+        "I love to hear that! Want to share more?"
+    ],
+    "negative": [
+        "I'm here for you. Want to talk about it?", 
+        "That sounds tough. What happened?"
+    ],
+    "neutral": [
+        "Gotcha. Anything interesting going on?", 
+        "Fair enough. Anything on your mind?"
+    ]
 }
 
 
