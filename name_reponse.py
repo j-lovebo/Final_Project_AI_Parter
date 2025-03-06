89abead05601a15edb4565c3f50ae247f61eab9f
j-lovebo <jlolivas@calpoly.edu>
2025-03-05 17:44:34 -0800

    name reponse

---
+++ name_reponse.py
@@ -0,0 +1,11 @@
+response2=str(input("What is your name?"))
+
+def name_response(userResponse):
+    print ("Wow such a beautiful name.")
+
+
+def main():
+    name_response(response2)
+
+if __name__ == "__main__":
+    main()
