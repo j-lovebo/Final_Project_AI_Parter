562a8f472fec14d5572f657fd17380dfa0e7e3a3
j-lovebo <jlolivas@calpoly.edu>
2025-03-09 15:19:05 -0700

    petclass

--- Pet_Class.py
+++ Pet_Class.py
@@ -1,7 +1,11 @@
 class Pet:
-    def __init__(self, name, pet_type):
+    def __init__(self, name, pet_type, color, personality):
         self.name = name
         self.pet_type = pet_type
+        self.color = color
+        self.personality = personality
 
     def __repr__(self):
-        return f"Pet(name={self.name}, pet_type={self.pet_type})"
+        return f"Pet(name={self.name}, pet_type={self.pet_type}, color={self.color}, personality={self.personality})"
+
+
